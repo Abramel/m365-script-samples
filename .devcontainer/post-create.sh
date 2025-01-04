8868669d9c03ae973d78993da31edd8cf95cb493
Abramel <149102473+Abramel@users.noreply.github.com>
2025-01-04 09:38:58 +0100

    Delete .devcontainer directory

--- .devcontainer/post-create.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# copy directory of plugins
-sudo cp -r /workspaces/script-samples/docfx/plugins /opt/docfx
--- .devcontainer/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# npm modules
-npm install -g http-server
